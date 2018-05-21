e3faccdad36554ea1b5b086a88999973763585e7
Toranian <mediamanmorrow@gmail.com>
2018-05-20 22:57:07 -0700

    Added a system to run a file if selected

--- file_finder.py
+++ file_finder.py
@@ -1,6 +1,7 @@
 import os, getpass
 from pathlib import Path
-import re
+from sys import platform
+import time
 
 # Helpful variables and home directory
 home_dir = str(Path.home()) # Home directory
@@ -8,40 +9,32 @@ os.chdir(home_dir)          # Set path to home directory
 working_dir = home_dir      # Working directory set to home directory by default
 all_dirs = []
 
+# Operating system dependant variable
+if platform == "linux" or platform == "darwin":
+    slash = "/"
+    execute_file = "./"
+else:
+    slash = "\\"
+    execute_file = "call"
+
 # Go back one directory
-def back(word):
-    word = word[::-1]               # Inverts string
-    index = word[1:].find('\\') + 1 # Finds "\" char and indexes it
-    word = word[index:]             # Gets word without the ending
-    word = word[::-1]               # Flips the string back
-    return word                     # Returns the word
+def back(directory):
+    directory = directory[::-1]               # Inverts string
+    index = directory[1:].find(slash) + 1 # Finds "\" char and indexes it
+    directory = directory[index:]             # Gets word without the ending
+    directory = directory[::-1]               # Flips the string back
+    return directory                     # Returns the word
 
-# Some commands to change or choose directory
-def change_directory():
-    global back_num, home_dir, working_dir
+# Help the user
+def help_func():
+    print("""
 
-    # Command input
-    command = input("select: ").lower()        # Command, lowercased to
-    command.replace(" ", "")                   # Replace all whitespace with nothing
-
-    if command== "quit" or command[0] == "q": quit() # Exit the program
-
-    if command == "help" or command[0] == "h": # Displays text that may be useful
-        print("""
-        1. Try typing the number of the file you want to open
-        2. Type [cd] to change the change the directory
-        3. Type [(b)ack] to revert to a previous directory
-        """)
-
-    if command == "back" or command[0] == "b": # Revert to a previous directory
-        working_dir = back(working_dir)
-        return working_dir
-
-    # Try to change the directory to a number
-    try:
-        return "{}\\{}".format(working_dir, str(files[int(command)]))
-    except:
-        return "Try (h)elp for a list of commands."
+    1. Try typing the number of the file you want to open
+    2. Type (q)uit to close the program
+    3. Type (b)ack to revert to a previous directory
+    """)
+    wait = input("""
+    Press enter to continue""")
 
 # Removes hidden files
 def clean_files(d_files):
@@ -50,6 +43,53 @@ def clean_files(d_files):
         if str(f[0]) != ".":
             c_files.append(f)
     return c_files
+
+# Some commands to change or choose directory
+def change_directory():
+    global working_dir
+    files = clean_files(os.listdir(working_dir)) # CWD of cleaned files
+
+    # Command input
+    command = ""
+    while len(command) == 0:
+        command = input("select: ").lower()        # Command, lowercased to
+        command.replace(" ", "")                   # Replace all whitespace with nothing
+
+    if command== "quit" or command[0] == "q": quit() # Exit the program
+
+    if command == "help" or command[0] == "h": # Displays text that may be useful
+        help_func()
+
+    if command == "back" or command[0] == "b": # Revert to a previous directory
+        working_dir = back(working_dir)
+
+
+    # Try to change the directory to a number
+    if command.isdigit():
+
+        file_name = str(files[int(command)])
+
+        # Run file if it is not a folder
+        if "." in file_name:
+            try:
+                os.system("{} {}".format(execute_file, file_name))
+                print("ran file")
+                time.sleep(1)
+                return back(working_dir)
+            except: pass
+
+        # Return the new directory
+        try:
+            return "{}{}{}".format(working_dir, slash, str(files[int(command)]))
+        except:
+            help_func()
+
+    # Try to retun the working directory, but if it fails, return the previous
+    # working_dir
+    try:
+        return working_dir
+    except:
+        return back(working_dir)
 
 # List all the files in the working_dir
 while True:
@@ -69,4 +109,12 @@ while True:
 
     # Current working directory
     working_dir = change_directory()
-    os.chdir(working_dir)
+
+    try:
+        os.chdir(working_dir)
+    except:
+        try:
+            os.chdir(back(working_dir))
+        except:
+            pass
+        help_func()
